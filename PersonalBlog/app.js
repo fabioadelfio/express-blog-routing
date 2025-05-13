e35234e86ed98004b01e5b44cfb70a4bd81da039
fabioadelfio <adelfiofabio07@gmail.com>
2025-05-13 14:12:53 +0200

    add data

--- PersonalBlog/app.js
+++ PersonalBlog/app.js
@@ -1,6 +1,7 @@
 const express = require(`express`);
 const app = express();
-const port = 3000;
+const { posts, appURL, appPort } = require(`./data/db`);
+
 
 app.use(express.static(`public`));
 
@@ -8,47 +9,11 @@ app.get("/", (req, res) => {
     res.send(`Server del mio blog`);
 });
 
-const posts = [
-    {
-        titolo: `Ciambellone`,
-        contenuto: `Ricetta ciambellone`,
-        immagine: `/img/ciambellone.jpeg`,
-        tags: [`ciambellone`, `dolce`],
-    },
-
-    {
-        titolo: `Cracker Barbabietola`,
-        contenuto: `Ricetta cracker barbabietola`,
-        immagine: `/img/cracker_barbabietola.jpeg`,
-        tags: [`cracker`, `barbabietola`],
-    },
-
-    {
-        titolo: `Pane Fritto Dolce`,
-        contenuto: `Ricetta pane fritto dolce`,
-        immagine: `/img/pane_fritto_dolce.jpeg`,
-        tags: [`pane`, `fritto`, `dolce`],
-    },
-
-    {
-        titolo: `Pasta Barbabietola`,
-        contenuto: `Ricetta pasta barbabietola`,
-        immagine: `/img/pasta_barbabietola.jpeg`,
-        tags: [`pasta`, `barbabietola`],
-    },
-
-    {
-        titolo: `Torta Paesana`,
-        contenuto: `Ricetta torta paesana`,
-        immagine: `/img/torta_paesana.jpeg`,
-        tags: [`torta`, `dolce`, `paese`],
-    }
-];
 
 app.get(`/bacheca`, (req, res) => {
     res.json({ posts });
 });
 
-app.listen(port, () => {
-    console.log(`Server avviato su http://localhost:${port}`);
+app.listen(appPort, () => {
+    console.log(`Server listening on ${appURL}`);
 })
